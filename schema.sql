041eabbb0a2754236f5966aad30a8850f905b49c
Lukas <n.lukas265@gmail.com>
2025-06-24 18:55:44 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,20 @@
--- Create your database schema here
+create type polymorphic.item_type as enum (
+  'TOPIC'
+);
+
+create table polymorphic.relational_items (
+  id serial primary key,
+
+  type polymorphic.item_type not null default 'TOPIC'::polymorphic.item_type,
+  updated_at timestamptz not null default now()
+);
+
+create table polymorphic.relational_topics (
+  id int primary key references polymorphic.relational_items,
+  title text not null
+);
+
+comment on table polymorphic.relational_items is $$
+  @interface mode:relational type:type
+  @type TOPIC references:relational_topics
+  $$;
